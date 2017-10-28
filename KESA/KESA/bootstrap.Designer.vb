c5c150f333c04ee67a686c7adad3f6ba305d1418
TeCeBe <artmaz64@gmail.com>
2017-10-28 15:26:12 +0200

    1 faza bootstrapa

--- KESA/KESA/bootstrap.Designer.vb
+++ KESA/KESA/bootstrap.Designer.vb
@@ -1,9 +1,9 @@
-﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
 Partial Class bootstrap
     Inherits System.Windows.Forms.Form
 
     'Formularz przesłania metodę dispose, aby wyczyścić listę składników.
-    <System.Diagnostics.DebuggerNonUserCode()> _
+    <System.Diagnostics.DebuggerNonUserCode()>
     Protected Overrides Sub Dispose(ByVal disposing As Boolean)
         Try
             If disposing AndAlso components IsNot Nothing Then
@@ -20,9 +20,31 @@ Partial Class bootstrap
     'UWAGA: następująca procedura jest wymagana przez Projektanta formularzy systemu Windows
     'Możesz to modyfikować, używając Projektanta formularzy systemu Windows. 
     'Nie należy modyfikować za pomocą edytora kodu.
-    <System.Diagnostics.DebuggerStepThrough()> _
+    <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.Label1 = New System.Windows.Forms.Label()
         Me.SuspendLayout()
+        '
+        'Timer1
+        '
+        Me.Timer1.Interval = 3000
+        '
+        'Label1
+        '
+        Me.Label1.Anchor = CType(((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.Label1.BackColor = System.Drawing.Color.Transparent
+        Me.Label1.Font = New System.Drawing.Font("Segoe UI Light", 36.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(238, Byte))
+        Me.Label1.ForeColor = System.Drawing.Color.White
+        Me.Label1.Location = New System.Drawing.Point(-31, 671)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(1342, 65)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Tryb odzyskiwania - R"
+        Me.Label1.TextAlign = System.Drawing.ContentAlignment.TopCenter
+        Me.Label1.Visible = False
         '
         'bootstrap
         '
@@ -30,6 +52,7 @@ Partial Class bootstrap
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.BackColor = System.Drawing.Color.Black
         Me.ClientSize = New System.Drawing.Size(1280, 768)
+        Me.Controls.Add(Me.Label1)
         Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
         Me.Name = "bootstrap"
         Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
@@ -39,4 +62,6 @@ Partial Class bootstrap
 
     End Sub
 
+    Friend WithEvents Timer1 As Timer
+    Friend WithEvents Label1 As Label
 End Class
--- KESA/KESA/bootstrap.vb
+++ KESA/KESA/bootstrap.vb
@@ -1,3 +1,28 @@
 ﻿Public Class bootstrap
+    Dim installer As Boolean = False
+    Private Sub bootstrap_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        'Odczyt stanu instalacji
+        Try
+            If My.Computer.FileSystem.ReadAllText("C:\KESA\bootstrap\state.conf") = "1" Then
+                Timer1.Start()
+            End If
+        Catch ex As Exception
+            installer = True
+            Timer1.Interval = 5000
+            Label1.Show()
+            Label1.Text = "Inicjowanie instalatora, proszę czekać..."
+            Cursor.Hide()
+            Timer1.Start()
+        End Try
+    End Sub
 
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        'Animacja + uruchomienie panelu logowania
+    End Sub
+
+    Private Sub bootstrap_KeyDown(sender As Object, e As KeyEventArgs) Handles Me.KeyDown
+        If e.KeyCode = Keys.R Then
+            'Uruchomienie recovery
+        End If
+    End Sub
 End Class
